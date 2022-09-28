dd76146341e5344012c958d3ba53b1df5738b7b3
Bodil Hundevad <bodil@hundevad.de>
2022-09-28 17:30:55 +0700

    new seeds

--- Gemfile
+++ Gemfile
@@ -75,3 +75,6 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+#add faker gem to generate fake data
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Note.destroy_all
+puts "destroyed old notes"
+Relationship.destroy_all
+puts "destroyed old relationships"
+
+puts "creating notes"
+ruby = Note.create(content: "Ruby is an interpreted, high-level, general-purpose programming language which supports multiple programming paradigms.")
+rails = Note.create(content: "Rails is a full-stack framework. It ships with all the tools needed to build amazing web apps on both the front and back end.")
+rails_seed_faker = Note.create(content: 'require "faker" 100.times do Restaurant.create(name: Faker::Restaurant.new, ratings: 5) end' )
+rails_new = Note.create(content: "Create a new rails file")
+puts "#{Note.count} notes creates"
+
+puts "creating relationships"
+Relationship.create(parent: ruby, child: rails)
+Relationship.create(parent: rails, child: rails_seed_faker)
+Relationship.create(parent: rails, child: rails_new)
+puts "#{Relationship.count} relationships creates"
