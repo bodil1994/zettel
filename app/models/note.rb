530f4ded851cffb90903d8a55b5ace0d2e2dcc76
Bodil Hundevad <bodil@hundevad.de>
2022-09-28 18:33:36 +0700

    new relationships between notes and and user

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,5 @@
 class Note < ApplicationRecord
+  belongs_to :user
   has_many :parent_relationships, foreign_key: :child_id, class_name: "Relationship"
   has_many :parents, through: :parent_relationships
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,19 @@
 require 'faker'
-
+User.destroy_all
+puts "destroyed old users"
 Note.destroy_all
 puts "destroyed old notes"
 Relationship.destroy_all
 puts "destroyed old relationships"
 
+puts "creating users"
+user_bodil = User.create(email: "bodil@hundevad.de", password: "123456")
+
 puts "creating notes"
-ruby = Note.create(content: "Ruby is an interpreted, high-level, general-purpose programming language which supports multiple programming paradigms.")
-rails = Note.create(content: "Rails is a full-stack framework. It ships with all the tools needed to build amazing web apps on both the front and back end.")
-rails_seed_faker = Note.create(content: 'require "faker" 100.times do Restaurant.create(name: Faker::Restaurant.new, ratings: 5) end' )
-rails_new = Note.create(content: "Create a new rails file")
+ruby = Note.create(user: user_bodil, content: "Ruby is an interpreted, high-level, general-purpose programming language which supports multiple programming paradigms.")
+rails = Note.create(user: user_bodil, content: "Rails is a full-stack framework. It ships with all the tools needed to build amazing web apps on both the front and back end.")
+rails_seed_faker = Note.create(user: user_bodil, content: 'require "faker" 100.times do Restaurant.create(name: Faker::Restaurant.new, ratings: 5) end' )
+rails_new = Note.create(user: user_bodil, content: "Create a new rails file")
 puts "#{Note.count} notes creates"
 
 puts "creating relationships"
