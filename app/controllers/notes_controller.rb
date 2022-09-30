1731dc71caf62bec9fdef727d82e74843b8b02d3
Bodil Hundevad <bodil@hundevad.de>
2022-09-30 20:47:44 +0700

    styled note form including focus

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,26 @@
 class NotesController < ApplicationController
   def index
+    @notes = current_user.notes
   end
+
   def new
     @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    @note.user = current_user
+
+    if @note.save!
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content)
   end
 end
