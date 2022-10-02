a31536d4dcbd0b3ebd303569c02da70e365c41c5
Bodil Hundevad <bodil@hundevad.de>
2022-10-02 18:34:40 +0700

    added focus to next input on enter button

--- app/javascript/controllers/formfocus_controller.js
+++ app/javascript/controllers/formfocus_controller.js
@@ -1,29 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="formfocus"
 export default class extends Controller {
 
   connect () {
-    console.log('Connected');
     this.next();
   }
-
 
   next () {
 
     let inputs = this.element.querySelectorAll('[id^="note"],[id*=" note"]');
-    console.log(inputs[1]);
 
     for (let i = 0; i < inputs.length; i++) {
-      // next add function to change focus
-      console.log(inputs[i]);
       let input = inputs[i];
+      // lastinput not really needed as textelement default of enter is not submit
+      let lastinput = (input === inputs[inputs.length - 1])
+
       const enter = function(event) {
-        if (event.keyCode === 13) {
+        if (event.keyCode === 13 && !lastinput) {
           event.preventDefault();
-          console.log("enter pressed")
-        } else {
-          event.keyCode
+          inputs[i+1].focus();
         }
       }
       input.addEventListener('keyup', enter, false)
@@ -31,18 +26,4 @@ export default class extends Controller {
       input.addEventListener('keydown', enter, false)
     }
   }
-
-    // let inputKeyup = (event.target.parentElement.querySelector('input') !== null)
-    // console.log(inputKeyup)
-    // let textareaKeyup = (event.target.parentElement.querySelector('textarea') !== null)
-    // console.log(textareaKeyup)
-    // // is next
-    // let relevantKeyup = (textareaKeyup || inputKeyup)
-    // // // for value if null: let b = a ?? "test"; ?? means is nullish
-    //  console.log(relevantKeyup)
-
-    // if (relevantKeyup && event === 13 ) {
-
-
-    // }
 }
