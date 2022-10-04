ffcb9823670d8322571229c557f1ab5389814228
Bodil Hundevad <bodil@hundevad.de>
2022-10-04 21:10:14 +0700

    new method decendants and ancestors

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,4 +5,12 @@ class Note < ApplicationRecord
 
   has_many :child_relationships, foreign_key: :parent_id, class_name: "Relationship"
   has_many :children, through: :child_relationships
+
+  def descendants
+    self.children | self.children.map(&:descendants).flatten
+  end
+
+  def ancestors
+    self.parents | self.parents.map(&:ancestors).flatten
+  end
 end
